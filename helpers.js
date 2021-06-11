4fd7881dd50c6f25d6480994ae32658938d54b2e
Alpha1337k <ozziekruithof@gmail.com>
2021-06-11 19:39:23 +0200

    Feat: export button

--- helpers.js
+++ helpers.js
@@ -5,3 +5,18 @@ function clearcanvas() {
 	ctx.beginPath();
 	console.log("cleared");
 }
+
+function save_as_json() {
+	update_data();
+	var savedata = {
+		"variables" 	: g_variables,
+		"constants" 	: g_constants,
+		"start"			: g_start,
+		"angle_const"	: g_angle_const,
+		"angle_start"	: g_start_angle,
+		"rules"			: g_rules,
+		"depth"			: g_depth,
+		"scale"			: document.getElementById("need_scale").checked
+	};
+	document.getElementById("exporttext").innerHTML = JSON.stringify(savedata);
+}
