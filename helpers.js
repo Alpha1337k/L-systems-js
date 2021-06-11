5d07ae0e8a20e446c0d884668f35db2035caecac
Alpha1337k <ozziekruithof@gmail.com>
2021-06-11 17:44:55 +0200

    Fix: now clears better

--- helpers.js
+++ helpers.js
@@ -2,5 +2,6 @@ function clearcanvas() {
 	let c = document.getElementById("cv");
 	let ctx = c.getContext("2d");
 	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.beginPath();
 	console.log("cleared");
 }
